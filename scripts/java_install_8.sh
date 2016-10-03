6ff7921e203bb0bd6933ac35d17b062f0a9a44d9
alvarezbruned <albert@alvarezbruned.com>
2016-10-03 12:58:10 -0500

    Update java_install_8.sh

--- scripts/java_install_8.sh
+++ scripts/java_install_8.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-add-apt-repository ppa:webupd8team/java -y
+apt-get install software-properties-common -y
+add-apt-repository -y ppa:webupd8team/java
 apt-get update
-apt-get install oracle-java8-installer -y
-apt-get install oracle-java8-set-default -y
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+apt-get -y install oracle-java8-installer
+java -version
