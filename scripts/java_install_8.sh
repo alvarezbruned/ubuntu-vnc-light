c336fea334669e159c6fb3974d9c8d9ce006ea96
alvarezbruned <albert@alvarezbruned.com>
2016-10-14 19:12:02 +0200

    Update java_install_8.sh

--- scripts/java_install_8.sh
+++ scripts/java_install_8.sh
@@ -5,4 +5,3 @@ apt-get update
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 apt-get -y install oracle-java8-installer
-java -version
