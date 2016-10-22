e421963b485a42096e83cf0b700cf2ca8d875a21
alvarezbruned <albert@alvarezbruned.com>
2016-10-22 11:53:18 +0200

    Update vnc_startup.sh

--- scripts/vnc_startup.sh
+++ scripts/vnc_startup.sh
@@ -19,6 +19,7 @@ echo -e "\n------------------ VNC environment started ------------------"
 echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $VNC_IP:$VNC_PORT"
 echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$VNC_IP:$NO_VNC_PORT/vnc_auto.html?password=..."
 cat bash.txt >> /etc/bash.bashrc
+sed -i -e '1iexport TERM=xterm\' ~/.bashrc
 for i in "$@"
 do
 case $i in
